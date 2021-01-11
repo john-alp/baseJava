d279521018dc46b023ae610a6d72c9c2ba97b0bc
Evgeny Solomatin <john-alp@mail.ru>
2021-01-11 11:24:37 +0300

    delLesson

---
+++ src/com/webmany/webapp/storage/AbstractPathStorage.java
@@ -0,0 +1,122 @@
+package com.webmany.webapp.storage;
+
+import com.webmany.webapp.exception.StorageException;
+import com.webmany.webapp.model.Resume;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public abstract class AbstractPathFileStorage extends AbstractStorage<Path> {
+    private Path directory;
+
+    protected abstract void doWrite(Resume resume, OutputStream os) throws IOException;
+    protected abstract Resume doRead(InputStream is) throws IOException;
+
+    protected AbstractPathFileStorage(String dir) {
+        this.directory = Paths.get(dir);
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
+            throw new IllegalArgumentException(dir + " is not directory or is not writable");
+        }
+    }
+
+    @Override
+    public void clear() {
+        getFileList().forEach(this::doDelete);
+    }
+
+    @Override
+    public int size() {
+        return (int) getFileList().count();
+    }
+
+    @Override
+    protected Path getSearchKey(String uuid) {
+        return directory.resolve(uuid);
+    }
+
+    @Override
+    protected boolean isExist(Path path) {
+        return Files.isRegularFile(path);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, Path path) {
+        try {
+            doWrite(resume, new BufferedOutputStream(Files.newOutputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("File write error", resume.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected void doSave(Resume resume, Path path) {
+        try {
+            Files.createFile(path);
+            // doWrite(resume, file);
+        } catch (IOException e) {
+            throw new StorageException("Couldn't create file "
+                    + path, path.getFileName().toString(), e);
+
+        }
+        doUpdate(resume, path);
+    }
+
+
+    @Override
+    protected Resume doGet(Path path) {
+        try {
+            return doRead(new BufferedInputStream(Files.newInputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("File read error",
+                    path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("File delete error", path.getFileName().toString(), e);
+        }
+    }
+
+
+    @Override
+    protected List<Resume> doCopyAll() {
+       return getFileList().map(this::doGet).collect(Collectors.toList());
+    }
+
+    private String getFileName(Path path) {
+        return path.getFileName().toString();
+    }
+
+    private Stream<Path> getFileList() {
+        try {
+            return Files.list(directory);
+        } catch (IOException e) {
+            throw new StorageException("Directory read error", e);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/webmany/webapp/storage/ObjectStreamPathStorage.java
@@ -0,0 +1,29 @@
+package com.webmany.webapp.storage;
+
+import com.webmany.webapp.exception.StorageException;
+import com.webmany.webapp.model.Resume;
+
+import java.io.*;
+
+public class ObjectStreamStorage extends AbstractFileStorage {
+    protected ObjectStreamStorage(File directory) {
+        super(directory);
+    }
+
+    @Override
+    protected void doWrite(Resume resume, OutputStream os) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(os)) {
+            oos.writeObject(resume);
+        }
+    }
+
+    @Override
+    protected Resume doRead(InputStream is) throws IOException {
+        try (ObjectInputStream ois = new ObjectInputStream(is)) {
+            return (Resume) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new StorageException("Error read resume", null, e);
+        }
+    }
+
+}
